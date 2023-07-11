4483ccdb7c7ae327bf4ca4f01caf3a5613a6dd73
Bryan Hickey <bjh@bjh.dev>
2023-07-11 16:59:17 +1000

    adjust image sizing

--- src/components/TextWithImage.tsx
+++ src/components/TextWithImage.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable max-len */
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-import { CustomPortableText } from '@/components/CustomPortableText'
-import { urlForImage } from '@/sanity/lib/image'
+import { CustomPortableText } from "@/components/CustomPortableText";
+import { urlForImage } from "@/sanity/lib/image";
 
 const TextWithImage = (props: any) => {
-  const { title, subtitle, image, text } = props.data
+  const { title, subtitle, image, text } = props.data;
   return (
     <div id="about" className="container py-12 md:py-20 lg:py-24">
       <div className="grid grid-cols-1 items-center lg:grid-cols-2 gap-6 lg:gap-6 xl:gap-12">
@@ -23,9 +23,9 @@ const TextWithImage = (props: any) => {
           </div>
           {text && <CustomPortableText value={text} paragraphClasses="mt-4" />}
         </div>
-        <div className="col-span-1 col-start-1 lg:col-span-1">
+        <div className="after:col-span-1 col-start-1 lg:col-span-1">
           <Image
-            className="max-w-lg mx-auto"
+            className="object-fill mx-auto"
             src={urlForImage(image).url()}
             alt={image.alt}
             width={900}
@@ -34,7 +34,7 @@ const TextWithImage = (props: any) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TextWithImage
+export default TextWithImage;
