76b86b4729c9aa04ded521d3eede652345705a8b
Bryan Hickey <bjh@bjh.dev>
2023-07-19 14:25:29 +1000

    add reviews

--- sanity/schemas/sections/textWithImage.tsx
+++ sanity/schemas/sections/textWithImage.tsx
@@ -1,29 +1,29 @@
-import { defineField, defineType } from 'sanity'
+import { defineField, defineType } from "sanity";
 
 export default defineType({
-  name: 'textWithImage',
-  title: 'Text With Image',
-  type: 'object',
+  name: "textWithImage",
+  title: "Text With Image",
+  type: "object",
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     }),
     defineField({
-      name: 'subtitle',
-      title: 'Subtitle',
-      type: 'string',
+      name: "subtitle",
+      title: "Subtitle",
+      type: "string",
     }),
     defineField({
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "image",
+      title: "Image",
+      type: "imageSimple",
     }),
     defineField({
-      name: 'text',
-      title: 'Text',
-      type: 'portableTextSimple',
+      name: "text",
+      title: "Text",
+      type: "portableTextSimple",
     }),
   ],
-})
+});
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,8 +1,86 @@
-import { CustomPortableText } from '@/components/CustomPortableText'
+import Image from "next/image";
+import Link from "next/link";
+import { FaStar } from "react-icons/fa";
 
-const Reviews = (props: any) => {
-  const { title, subtitle, shortDescription, leaveAReview } = props.data
-  // console.log('Reviews: ', props)
+import { CustomPortableText } from "@/components/CustomPortableText";
+
+const placeId = process.env.NEXT_PUBLIC_PLACES_ID;
+const apiKey = process.env.GOOGLE_KEY;
+const url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJM6hAERld1moRRG9fPzJdqA8&key=${apiKey}`;
+
+const StarRating = ({ rating }: { rating: number }) => {
+  return (
+    <div className="flex items-center space-x-1">
+      {rating >= 1 && <FaStar className="text-yellow-500" />}
+      {rating >= 2 && <FaStar className="text-yellow-500" />}
+      {rating >= 3 && <FaStar className="text-yellow-500" />}
+      {rating >= 4 && <FaStar className="text-yellow-500" />}
+      {rating >= 5 && <FaStar className="text-yellow-500" />}
+    </div>
+  );
+};
+type Review = {
+  author_name: string;
+  author_url: string;
+  language: string;
+  original_language: string;
+  profile_photo_url: string;
+  rating: number;
+  relative_time_description: string;
+  text: string;
+  time: number;
+  translated: boolean;
+};
+
+const ReviewCard = (props: Review) => {
+  return (
+    <a
+      target="_blank"
+      rel="noreferrer noopener"
+      href={props.author_url}
+      className="flex space-x-4 hover:bg-gray-700 p-4 rounded-lg"
+    >
+      <div className="flex-none">
+        <Image
+          className="w-16 h-16 rounded-full"
+          src={props.profile_photo_url}
+          alt={props.author_name}
+          width={120}
+          height={120}
+        />
+      </div>
+      <div className="flex flex-col space-y-2">
+        <div>
+          <p className="font-medium text-white">{props.author_name}</p>
+        </div>
+        <div>
+          <StarRating rating={props.rating} />
+        </div>
+        <div>
+          <p className="text-xs leading-5 text-gray-300 italic line-clamp-5">
+            {props.text}
+          </p>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+type ReviewsProps = {
+  data: {
+    title: string;
+    subtitle: string;
+    shortDescription: string;
+    leaveAReview: any;
+  };
+  reviews: Review[];
+};
+
+const Reviews = async (props: ReviewsProps) => {
+  const { title, subtitle, shortDescription, leaveAReview } = props.data;
+  const details = await fetch(url).then((res) => res.json());
+  const reviews = details.result.reviews;
+  console.log("review: ", reviews);
   return (
     <div className="bg-gray-900 py-12">
       <div className="container">
@@ -19,20 +97,12 @@ const Reviews = (props: any) => {
           </div>
           {shortDescription && <p className="text-white">{shortDescription}</p>}
         </div>
-        <div className="grid grid-cols-1 gap-2 lg:grid-cols-2 lg:gap-12 xl:grid-cols-4">
-          {/* {reviews.map((review) => (
+        <div className="py-12 grid grid-cols-1 gap-2 lg:grid-cols-2 lg:gap-12 xl:grid-cols-4">
+          {reviews.slice(0, 4).map((review: any, index: any) => (
             <div key={review.id} className="col-span-1">
-              <ReviewCard
-                id={review.id}
-                name={review.author_name}
-                text={review.text}
-                reviewLink={review.author_url}
-                time={review.relative_time_description}
-                rating={review.rating}
-                darkSection
-              />
+              <ReviewCard id={index} {...review} darkSection />
             </div>
-          ))} */}
+          ))}
         </div>
         <div className="mt-6 justify-between border-t-2 border-gray-800 pt-6 text-sm text-white lg:flex">
           {leaveAReview && (
@@ -44,7 +114,7 @@ const Reviews = (props: any) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
